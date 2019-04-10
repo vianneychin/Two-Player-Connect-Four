c86c0d90e00518a6d6ec82e1e9d80e75b726c4ad
Vianney Chin <vianneychin@Vianneys-MacBook-Pro.local>
2019-04-10 00:19:28 -0700

    Commit 5: Add Switch Player Function

--- js/main.js
+++ js/main.js
@@ -12,3 +12,10 @@ const blueToken = game.players[1]
 const setPlayer = () => {
     game.currentPlayer = game.players[0]
 }
+const switchPlayers = () =>{
+    if (game.currentPlayer === yellowToken) {
+        game.currentPlayer = blueToken
+    } else if (game.currentPlayer === blueToken) {
+        game.currentPlayer = yellowToken
+    }
+}
