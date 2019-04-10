6490a43f56473075a593c3fea386ce87ae5f56ad
Vianney Chin <vianneychin@Vianneys-MacBook-Pro.local>
2019-04-09 23:45:38 -0700

    Commit 4: Set Player

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log(123)
+const game = {
+    currentPlayer:{},
+    players:[
+        {player:"Yellow", color: "yellow"},
+        {player:"Blue", color:"blue"}
+    ]
+}
+const $circle = $(".circle")
+const $columns = $(`.column`)
+const yellowToken = game.players[0]
+const blueToken = game.players[1]
+const setPlayer = () => {
+    game.currentPlayer = game.players[0]
+}
